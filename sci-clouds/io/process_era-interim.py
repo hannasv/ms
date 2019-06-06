177cd49083d1b6c92ed990223a3396702b081ce9
hannasv <svennevikh@gmail.com>
2019-06-06 15:04:32 +0200

    WV copping era sesonal

--- sci-clouds/io/process_era-interim.py
+++ sci-clouds/io/process_era-interim.py
@@ -1,9 +1,9 @@
 import sci-clouds.helpers import DATA_REPO, VARIABLES
 
 
-class ProcessData:
+class ProcessEraData:
     """
-    Class which is ment to process data in a certain way.
+    Class which is ment to process era-intetim data in a certain way.
 
     1. Split into seasons.
     2. Split into train-, validation- and testdata.
@@ -23,18 +23,28 @@ class ProcessData:
             # TODO write file, use key in filename and dataset.dump to netcdf
             # To save the files.
 
-    def split_into_train_vaild_test_data():
+    def split_into_train_vaild_test_data(train_split, valid_split, test_split, last):
         """
         Do the train, validate, split on the data.
+
+        train_split: default 01.01.2008
+            The first date in training data.
+            trainingdata from 01.01.2008-31.12.2014 (In total 6yrs)
+
+        valid_split:
+            The first date in training data. Here 01.01.2015.
+            (01.01.2015-31.12.2016) (In total 2yrs)
+
+        test_split:
+            The test date in training data. Here ??.
+            (01.01.2017-31.12.2018) (In total 2yrs)
+
+        last: Default 31.12.2018.
+            Should be updated to 31.08.2019 when the dataset is available.
+
         """
         pass
 
-    def regrid_tcc():
-        """
-        Regrid the cloud mask to total cloud cover, which has the same spatial
-         resolution as era interim data.z<
-        """
-        pass
 
 
     def crop_era_interim():
