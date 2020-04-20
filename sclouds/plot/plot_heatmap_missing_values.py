047e017c16550898707843692fb29bde4e8b597e
Hanna Svennevik <svennevikh@gmail.com>
2020-04-20 22:51:11 +0200

    added a lot of plotting routines and set up notebook for reproducable plots

---
+++ sclouds/plot/plot_heatmap_missing_values.py
@@ -0,0 +1,85 @@
+import os
+import glob
+
+import numpy as np
+import xarray as xr
+import pandas as pd
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sclouds.helpers import (path_input, path_stats_results, VARIABLES,
+                                UNITS, LONGNAME)
+from sclouds.plot.helpers import (TEXT_WIDTH_IN, TEXT_HEIGHT_IN,
+                                    path_python_figures, import_matplotlib)
+
+matplotlib = import_matplotlib()
+
+def get_path(year, month, base = '/home/hanna/lagrings/ERA5_monthly/'):
+    month ="%2.2d" %month # includng leading zeros.
+    search_str = '{}*{}*tcc.nc'.format(year, month)
+    return glob.glob(os.path.join(base, search_str))
+
+def get_missing_hours(year, month):
+    files = get_path(year, month)
+
+    if len(files) == 0:
+        print("year: {}, month: {}".format(year, month))
+        return np.nan
+    else:
+        fil = files[0]
+        if month < 10:
+            month1 = "%2.2d" %month
+            month2 = "%2.2d" %(month+1)
+            year2 = year
+
+        elif month == 12:
+            year2 = year+1
+            month1 = month
+            month2="01"
+        else:
+            month1 = month
+            month2 = month + 1
+            year2  = year
+
+        data = xr.open_dataset(fil) # , decode_times = False
+        #print(data)
+        #print(data)
+        start = '{}-{}-01'.format(year, month1)
+        stop = '{}-{}-01'.format(year2, month2)
+
+        timearray = np.arange(start, stop, np.timedelta64(1,'h'), dtype='datetime64[ns]')
+        #print(len(timearray))
+        ll = data.time.values.astype(np.datetime64)
+
+        counter = 0
+        for element in timearray:
+            if element not in ll:
+                counter += 1
+
+        assert len(timearray) >= counter, "how, start {}, stop {}, "\
+                    "len timearray {}, counter {}".format(start, stop, len(timearray), counter)
+        return counter
+
+
+years = np.arange(2004, 2019)
+months = np.arange(1, 13)
+storage = {}
+for y in years:
+    storage[str(y)] = {}
+    for m in months:
+        storage[str(y)][str(m)] = get_missing_hours(y, m)
+
+
+df = pd.DataFrame.from_dict(storage)
+
+from sclouds.plot.helpers import TEXT_WIDTH_IN, path_python_figures
+fig, ax = plt.subplots(1, 1, figsize = (TEXT_WIDTH_IN, 0.5*TEXT_WIDTH_IN))
+ytikz = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+fig, ax = plt.subplots(1, 1, figsize = (TEXT_WIDTH_IN, 0.5*TEXT_WIDTH_IN))
+ax2 = sns.heatmap(df, linewidths=0.1, linecolor='white', vmax = 10,annot=True, # fmt="d",
+            cbar_kws={'extend':'max'}, ax = ax, yticklabels=ytikz)
+
+plt.subplots_adjust(hspace = 0.2, top=0.97, bottom=0.15, left = 0.15, right = 1.05)
+plt.xticks(rotation=45)
+fig.savefig(path_python_figures + 'heatmap_missing_values.pdf')
---
+++ sclouds/plot/plot_one_week_of_spatially_averaged_values.py
@@ -0,0 +1,99 @@
+""" Plotting routine used to plot subplots of spatially averages monthly means
+and filtered by land sea and both.
+"""
+import os
+import glob
+
+import numpy as np
+import xarray as xr
+
+from sclouds.helpers import (path_input, path_stats_results, VARIABLES,
+                                UNITS, LONGNAME)
+from sclouds.plot.helpers import (TEXT_WIDTH_IN, TEXT_HEIGHT_IN,
+                                    path_python_figures, import_matplotlib)
+n_rows = len(VARIABLES)
+n_cols = 1
+
+data = xr.open_dataset('/home/hanna/lagrings/results/stats/monthly_means.nc')
+
+fig, axes =  plt.subplots(nrows = n_rows, ncols = n_cols, sharex=True, sharey=False)
+fig.set_size_inches(w = TEXT_WIDTH_IN, h = TEXT_HEIGHT_IN - 1)
+plt.subplots_adjust(hspace = 0.2, top=0.97, bottom=0.03, left = 0.14, right = 0.97)
+
+mat = import_matplotlib()
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+
+def filter_data_in_period(start = '2012-07-01', stop  = '2012-07-07'):
+    """ Add a land and sea filter of all varibles to existing data frame.
+
+    Parameters
+    --------------
+    data : xr.Dataset
+        Containes for data you want to filter.
+
+    Returns
+    --------------
+    data : xr.Dataset
+        Spatially averaged filtered timeseries.
+    """
+    from sclouds.helpers import VARIABLES
+    from sclouds.io.filter import Filter
+    from sclouds.io.utils import get_xarray_dataset_for_period
+
+    data = get_xarray_dataset_for_period(start = start, stop = stop)
+
+    df_return = data.mean(['latitude', 'longitude']).copy()
+
+    for var in VARIABLES:
+        fs = Filter('sea').set_data(data, variable = var)
+        fl = Filter('land').set_data(data, variable = var)
+
+        df_return['land_{}'.format(var)] = fl.get_spatial_mean()
+        df_return['sea_{}'.format(var)]  = fs.get_spatial_mean()
+
+    return df_return
+
+
+for i in range(1, 13):
+    start = '2012-{:02d}-01'.format(i)
+    stop  = '2012-{:02d}-07'.format(i)
+
+    df = filter_data_in_period(start = start, stop  = stop)
+
+    n_rows = len(VARIABLES)
+    n_cols = 1
+
+    data = df
+    date = df.time.values
+
+    fig, axes =  plt.subplots(nrows = n_rows, ncols = n_cols)
+    fig.set_size_inches(w = TEXT_WIDTH_IN, h = TEXT_HEIGHT_IN - 1)
+    fig.autofmt_xdate()
+
+    plt.subplots_adjust(hspace = 0.5, top=0.97, bottom=0.1, left = 0.14, right = 0.97)
+
+    for var, ax in zip(VARIABLES, axes):
+        #if var != 'tcc':
+        vals   = data[var].values
+        f_land = data['land_{}'.format(var)].values
+        f_sea  = data['sea_{}'.format(var)].values
+        #date   = data['date_{}'.format(var)].values
+
+        ax.set_title(LONGNAME[var], fontsize = 14)
+        ax.plot(date, vals, label = '{}'.format('both'))
+        ax.plot(date, f_land, label = '{}'.format('land'))
+        ax.plot(date, f_sea, label = '{}'.format('sea'))
+
+        ax.set_ylabel('{} [{}]'.format(var, UNITS[var]))
+        ax.legend()
+        # rotate and align the tick labels so they look better
+
+
+        # use a more precise date string for the x axis locations in the
+        # toolbar
+        ax.fmt_xdata = mdates.DateFormatter('%Y-%m-%d')
+        #ax.set_title('fig.autofmt_xdate fixes the labels')
+
+    plt.savefig(path_python_figures + 'spatially_averaged_one_week_from_{}.png'.format(start))
