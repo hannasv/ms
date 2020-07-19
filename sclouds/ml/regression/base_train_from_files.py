31bc15af36a4d82e956bdd278215290e69d18b70
hannasv <svennevikh@gmail.com>
2020-07-19 16:36:23 +0200

    added more regression

--- sclouds/ml/regression/base_train_from_files.py
+++ sclouds/ml/regression/base_train_from_files.py
@@ -51,16 +51,40 @@ def get_config_from_model_name(name):
     order      = int(splits[-1])
     model_type = splits[0]
     temp_dict  = {'transform'  : transform,
-                  'sigmoid'    : sigmoid,
-                  'order'      : order,
-                  'start'      : '2004',
-                  'stop'       : '2013',
-                  'test_start' : '2014',
-                  'test_stop'  : '2018',
-                  'bias'       :  bias,
-                  'type'       :  model_type}
+                'sigmoid'    : sigmoid,
+                'order'      : order,
+                'start'      : '2004',
+                'stop'       : '2013',
+                'test_start' : '2014',
+                'test_stop'  : '2018',
+                'bias'       :  bias,
+                'type'       :  model_type}
 
     return temp_dict
+
+def get_list_of_files_to_regridd(model_name, path):
+    finished_files = glob.glob(path + f'results/ar/*performance*{model_name}*o{2}*')
+    lh_files = glob.glob(path+'ar_data/*.nc')
+    print(lh_files[0])
+    # Get search string from trained models. 
+    crop_files_finished = []
+    for fil in finished_files:
+        splits = fil.split('_')
+        lat = splits[-2]
+        lon = splits[-1][:-3]
+        crop_files_finished.append(f'{lon}_{lat}')
+        
+    list_to_regridd = []
+    # Crop_files = []
+    for fil in lh_files:
+        splits = fil.split('_')
+        lat = splits[-2]
+        lon = splits[-1][:-3]
+        search_key = f'{lat}_{lon}'
+
+        if not search_key in crop_files_finished:
+            list_to_regridd.append(fil)
+    return list_to_regridd
 
 def transform_X(X, lat, lon, data, order):
     X_train = np.zeros(X.shape)
@@ -90,102 +114,104 @@ def inverse_sigmoid(x):
     Parameteres
     -------------------
     x : array-like
-        Vector containing the values.
+      Vector containing the values.
 
     Returnes
     --------------------
     _ : array-like
-        The inverse sigmoid transform of x
+      The inverse sigmoid transform of x
     """
     return np.log(x/(1 - x + 0.1))
 
-path_transform = '/home/hanna/lagrings/results/stats/2014-01-01_2018-12-31/'
-path = '/home/hanna/lagrings/ar_data/'
 
+def train_ar_model(transform=False, bias=False, sig=False, order=0, overwrite_results = True):
+    path_transform = '/home/hanna/lagrings/results/stats/2014-01-01_2018-12-31/'
+    path = '/home/hanna/lagrings/ar_data/'
+    #print(bias)
+    #print(transform)
+    if transform and bias:
+        print('Not valid model....')
+        raise OSError('Not valid model config')
 
-# path_transform = '/home/hanna/lagrings/results/stats/2014-01-01_2018-12-31/'
-# path = '/home/hanna/lagrings/ar_data/'
+    # path_transform = '/home/hanna/lagrings/results/stats/2014-01-01_2018-12-31/'
+    # path = '/home/hanna/lagrings/ar_data/'
 
-lagr_path = '/uio/lagringshotell/geofag/students/metos/hannasv/'
-path_transform = '{}results/stats/2014-01-01_2018-12-31'.format(lagr_path)
+    lagr_path = '/uio/lagringshotell/geofag/students/metos/hannasv/'
+    path_transform = '{}results/stats/2014-01-01_2018-12-31'.format(lagr_path)
 
-path = '{}ar_data/'.format(lagr_path)
-path_ar_results = '{}/results/ar/models/'.format(lagr_path)
+    path = '{}ar_data/'.format(lagr_path)
+    path_ar_results = '{}/results/ar/'.format(lagr_path)
 
-latitude  = 30.0
-longitude = 5.25
-SPATIAL_RESOLUTION = 0.25
+    latitude  = 30.0
+    longitude = 5.25
+    SPATIAL_RESOLUTION = 0.25
 
-latitudes =  np.arange(30.0, 50.0 +SPATIAL_RESOLUTION, step = SPATIAL_RESOLUTION)
-longitudes = np.arange(-15, 25+SPATIAL_RESOLUTION, step = SPATIAL_RESOLUTION)
-base = '{}/results/stats/2014-01-01_2018-12-31/'.format(lagr_path)
+    latitudes =  np.arange(30.0, 50.0 +SPATIAL_RESOLUTION, step = SPATIAL_RESOLUTION)
+    longitudes = np.arange(-15, 25+SPATIAL_RESOLUTION, step = SPATIAL_RESOLUTION)
+    base = '{}/results/stats/2014-01-01_2018-12-31/'.format(lagr_path)
 
-transform = False
-if transform:
+    if transform:
+        ds_tcc = xr.open_dataset(base + 'stats_pixel_tcc_all.nc')
+        ds_r = xr.open_dataset(base +'stats_pixel_r_all.nc')
+        ds_q = xr.open_dataset(base+'stats_pixel_q_all.nc')
+        ds_t2m = xr.open_dataset(base+'stats_pixel_t2m_all.nc')
+        ds_sp = xr.open_dataset(base+'stats_pixel_sp_all.nc')
 
-    ds_tcc = xr.open_dataset(base + 'stats_pixel_tcc_all.nc')
-    ds_r = xr.open_dataset(base +'stats_pixel_r_all.nc')
-    ds_q = xr.open_dataset(base+'stats_pixel_q_all.nc')
-    ds_t2m = xr.open_dataset(base+'stats_pixel_t2m_all.nc')
-    ds_sp = xr.open_dataset(base+'stats_pixel_sp_all.nc')
+        stats_data = {'q':ds_q, 't2m': ds_t2m, 'r': ds_r, 'sp': ds_sp, 'tcc': ds_tcc}
 
-    stats_data = {'q':ds_q, 't2m': ds_t2m, 'r': ds_r, 'sp': ds_sp, 'tcc': ds_tcc}
+    explaination = ['q', 't2m', 'r', 'sp']
+    tr_e = []
+    tr_index = 4
 
-sig = False
-bias = False
-order =5
-model_type = 'AR'
+    if bias:
+        explaination.append('bias')
+        tr_e.append('bias')
+        #tr_index +=1
 
-explaination = ['q', 't2m', 'r', 'sp']
-tr_index = 3
+    full_name = generate_model_name('AR', bias, transform, sig, order)
+    config = get_config_from_model_name(full_name)
 
-if bias:
-    explaination.append('bias')
-    #tr_index += 1
+    full_name_tr = generate_model_name('TR', bias, transform, sig, order)
+    tr_config = get_config_from_model_name(full_name_tr)
+    
+    list_to_regridd = get_list_of_files_to_regridd(model_name=full_name, path=lagr_path)
+    print('Detected {} num files to regridd.'.format(len( list_to_regridd ))) 
+    for fil in list_to_regridd:
+        print(fil)
+        splits = fil.split('_')
+        latitude = splits[-2]
+        longitude = splits[-1][:-3]
 
-full_name = generate_model_name(model_type, bias, transform, sig, order)
-config = get_config_from_model_name(full_name)
+        data = xr.open_dataset(fil)
 
-full_name_tr = generate_model_name('TR', bias, transform, sig, order)
-tr_config = get_config_from_model_name(full_name_tr)
+        explain = explaination.copy()
+        tr_explain = tr_e.copy()
 
-#self.result_path = result_path
-if not os.path.exists(path_ar_results):
-    os.makedirs(path_ar_results)
-    path_ar_results = os.path.join(path_ar_results, full_name)
-    if not os.path.exists(os.path.join(path_ar_results, full_name)):
-            os.makedirs(os.path.join(path_ar_results, full_name))
-
-
-for latitude in latitudes:#[30.0, 30.25]: # , 30.5, 30.75, 31.0
-    for longitude in longitudes:#[0.0, 0.25, 0.5, 0.75, 1.0]:
-    #for latitude in latitudes:
-
-       explain = explaination.copy()
-       tr_explain = []
-
-       for o in range(0, order +1):
-            name    = full_name+'-o{}'.format(o)
-            tr_name = full_name_tr+'-o{}'.format(o)
+        for o in range(0, order +1):
+            name    = full_name+'-L{}'.format(o)
+            tr_name = full_name_tr+'-L{}'.format(o)
 
             w_filename        = '{}weights_{}_{}_{}.nc'.format(path_ar_results, name, longitude, latitude)
             p_filename        = '{}performance_{}_{}_{}.nc'.format(path_ar_results, name, longitude, latitude)
-
-            fil = 'all_vars_lat_lon_{}_{}.nc'.format(latitude, longitude)
-            data = xr.open_dataset(path+fil)
-            #if not os.path.exists(w_filename) and os.path.exists(p_filename):
+            #data = xr.open_dataset(path+fil)
             if o > 0:
-               explain.append('O{}'.format(o))
-               tr_explain.append('O{}'.format(o))
+                explain.append('O{}'.format(o))
+                tr_explain.append('O{}'.format(o))
             start_time = timeit()
-            X_train, y_train = dataset_to_numpy_order(dataset = data.sel(time = slice('2004', '2013')),
-                                      order = order,  bias = bias)
 
+            X_train, y_train = dataset_to_numpy_order(dataset = data.sel(time = slice('2004', '2013')),
+                            order = order,  bias = bias)
+            #print(X_train[0, :])
             X_test, y_test   = dataset_to_numpy_order(dataset = data.sel(time = slice('2014', '2018')),
-                                      order = order,  bias = bias)
-            if transform:
+                            order = order,  bias = bias)
+            #print('transform {}'.format(transform))
+            #print(bias)
+            if transform: # and not bias):# or (not transform and bias):
                 X_train = transform_X(X_train, lat = latitude, lon=longitude, data=stats_data, order=o)
                 X_test = transform_X(X_test, lat = latitude, lon=longitude, data=stats_data, order=o)
+            #else:
+            #    print('Not valid model....')
+            #    raise OSError('Not valid model config')
 
             if sig:
                 y_train = inverse_sigmoid(y_train)
@@ -199,8 +225,8 @@ for latitude in latitudes:#[30.0, 30.25]: # , 30.5, 30.75, 31.0
             weights_dict = {}
             weights_tr_dict = {}
 
-            Xtr, ytr =  drop_nans(X_train[:, :int(4+o)], y_train)
-            Xte, yte =  drop_nans(X_test[:, :int(4+o)], y_test)
+            Xtr, ytr =  drop_nans(X_train[:, :int(tr_index+o)], y_train)
+            Xte, yte =  drop_nans(X_test[:, :int(tr_index+o)], y_test)
 
             if sig:
                 yte = sigmoid(yte)
@@ -214,9 +240,9 @@ for latitude in latitudes:#[30.0, 30.25]: # , 30.5, 30.75, 31.0
 
             if o > 0:
                 # updatig predictors
-                Tr_Xtr =  Xtr[:, 4:]
-                Tr_Xte =  Xte[:, 4:]
-
+                Tr_Xtr =  Xtr[:, tr_index:]
+                Tr_Xte =  Xte[:, tr_index:]
+                print(Tr_Xtr.shape)
                 coeffs_tr   = fit_pixel(Tr_Xtr, ytr)
 
                 y_test_pred_tr  = predict_pixel(Tr_Xte, coeffs_tr)
@@ -242,6 +268,8 @@ for latitude in latitudes:#[30.0, 30.25]: # , 30.5, 30.75, 31.0
             mae_train = mean_squared_error(y_train_pred, ytr)
 
             ##################### Adding the autoregressive model
+            #print(coeffs)
+            #print(explaination)
             weights_dict['coeffs'] = (['weights'], coeffs.flatten())  # 'latitude', 'longitude',
 
             eval_dict['mse_test']  = mse_test[0]   #(['latitude', 'longitude'],)
@@ -262,7 +290,8 @@ for latitude in latitudes:#[30.0, 30.25]: # , 30.5, 30.75, 31.0
             ###################### Adding traditional model
             if o > 0:
                 weights_tr_dict['coeffs'] = (['weights'], coeffs_tr.flatten())  # 'latitude', 'longitude',
-
+                print(weights_tr_dict)
+                print(tr_explain)
                 eval_tr_dict['mse_test']  = mse_test_tr[0]   #(['latitude', 'longitude'],)
                 eval_tr_dict['mse_train'] = mse_train_tr[0]
 
@@ -278,6 +307,20 @@ for latitude in latitudes:#[30.0, 30.25]: # , 30.5, 30.75, 31.0
                 eval_tr_dict.update(tr_config)
                 weights_tr_dict.update(tr_config)
 
+                w_tr_filename        = '{}/weights_{}_{}_{}.nc'.format(path_ar_results, tr_name, longitude, latitude)
+                p_tr_filename        = '{}/performance_{}_{}_{}.nc'.format(path_ar_results, tr_name, longitude, latitude)
+
+                ds = xr.Dataset(weights_tr_dict, coords={'latitude':  (['latitude'],  [latitude]),
+                                            'longitude': (['longitude'], [longitude]),
+                                            'weights':   (['weights'],   tr_explain )
+                                            })
+                ds.to_netcdf(w_tr_filename)
+
+                ds = xr.Dataset(eval_tr_dict, coords={'latitude': (['latitude'],   [latitude]),
+                                         'longitude': (['longitude'], [longitude])
+                                         })
+                ds.to_netcdf(p_tr_filename)
+
             stop_time = timeit()
             #print(stop_time - start_time)
             eval_dict['time_elapsed_seconds'] = (stop_time - start_time) #(['latitude', 'longitude'], )
@@ -285,30 +328,17 @@ for latitude in latitudes:#[30.0, 30.25]: # , 30.5, 30.75, 31.0
             w_filename        = '{}weights_{}_{}_{}.nc'.format(path_ar_results, name, longitude, latitude)
             p_filename        = '{}performance_{}_{}_{}.nc'.format(path_ar_results, name, longitude, latitude)
             ds = xr.Dataset(weights_dict, coords={'latitude':  (['latitude'],  [latitude]),
-                                              'longitude': (['longitude'], [longitude]),
-                                              'weights':   (['weights'],   explain )
-                                              })
+                                    'longitude': (['longitude'], [longitude]),
+                                    'weights':   (['weights'],   explain )
+                                    })
             ds.to_netcdf(w_filename)
 
             ds = xr.Dataset(eval_dict, coords={'latitude': (['latitude'],   [latitude]),
-                                           'longitude': (['longitude'], [longitude])
-                                           })
+                                 'longitude': (['longitude'], [longitude])
+                                 })
             ds.to_netcdf(p_filename)
-            ################# Storing traditional model
-            if o > 0:
-                w_tr_filename        = '{}/weights_{}_{}_{}.nc'.format(path_ar_results, tr_name, longitude, latitude)
-                p_tr_filename        = '{}/performance_{}_{}_{}.nc'.format(path_ar_results, tr_name, longitude, latitude)
+            print('finished calibrating bias {}, sigmoid {}, Transform {}, order/Lag {} - ({}, {})'.format(bias, sig, transform, o, longitude, latitude))
+        else:
+            print('Model config already calibrated bias {}, sigmoid {}, Transform {}, order/Lag {} - ({}, {})'.format(bias, sig, transform, o, longitude, latitude))
 
-                ds = xr.Dataset(weights_tr_dict, coords={'latitude':  (['latitude'],  [latitude]),
-                                                  'longitude': (['longitude'], [longitude]),
-                                                  'weights':   (['weights'],   tr_explain )
-                                                  })
-                ds.to_netcdf(w_tr_filename)
 
-                ds = xr.Dataset(eval_tr_dict, coords={'latitude': (['latitude'],   [latitude]),
-                                               'longitude': (['longitude'], [longitude])
-                                               })
-                ds.to_netcdf(p_tr_filename)
-            print('finished false, false, false({}, {})'.format(longitude, latitude))
-            #else:
-            #    print('file {} and {} exist'.format(p_filename, w_filename) )
